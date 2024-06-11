d6c53775b57ffe43fae59510874610c2ac8ffdaf
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-11 16:29:28 +0000

    test 2

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,7 +3,7 @@ import React from "react";
 function Review({ review, handleClick }) {
     return (
         <div className="review">
-            <p className="author" id={"author-" + review.id}>Author: {review.name}</p>
+            <p className="author" id="author-1">Author: {review.name}</p>
             <p className="job">Job: {review.job}</p>
             <p className="info">Info: {review.text}</p>
             <img className="person-img" style={{height: '400px'}} src={review.image} />
